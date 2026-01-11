6eb54385d25c67580eefd51cde0bbf2c274fde03
svivek19 <svivek030503@gmail.com>
2026-01-11 13:07:50 +0530

    chore: initial project setup with express and mongodb

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "DigiParivaar Backend Running" });
+});
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
