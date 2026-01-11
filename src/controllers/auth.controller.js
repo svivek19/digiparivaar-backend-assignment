5dea0a62f03fcb44c24d6c149f2c06d439d8a442
svivek19 <svivek030503@gmail.com>
2026-01-11 13:11:45 +0530

    feat: implement otp auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { generateOTP, saveOTP, verifyOTP } = require("../utils/otpStore");
+
+exports.requestOTP = async (req, res) => {
+  const { phone } = req.body;
+
+  if (!phone) {
+    return res.status(400).json({ message: "Phone is required" });
+  }
+
+  const otp = generateOTP();
+  saveOTP(phone, otp);
+
+  return res.json({ otp }); // mocked OTP
+};
+
+exports.verifyOTP = async (req, res) => {
+  const { phone, otp } = req.body;
+
+  if (!verifyOTP(phone, otp)) {
+    return res.status(401).json({ message: "Invalid or expired OTP" });
+  }
+
+  let user = await User.findOne({ phone });
+  if (!user) {
+    user = await User.create({ phone });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.json({ token });
+};
